e13ba41db3f9d5b9450586e2bd8da4a24897c04e
Jun206 <209410793@gms.tku.edu.tw>
2023-10-04 21:58:11 +0800

    2

---
+++ demo/w04_93_Blog/utils/data/data.sql
@@ -0,0 +1,39 @@
+CREATE TABLE card_93 (
+  id int NOT NULL PRIMARY KEY,
+  title varchar(255),
+  description varchar(255),
+  categories varchar(255),
+  img_remote varchar(255),
+  img varchar(255)
+)
+
+INSERT INTO card_93 (
+  id, title, description, categories, img_remote, img
+) VALUES (
+  1, 'seven reasons why coffee is awesome', 'Lorem ipsum dolor, sit amet consectetur adipisicing elit.', 'lifestyle', '/images/photo-1.jpg', ''
+),
+(
+  2, 'travel to paris', 'Lorem ipsum dolor, sit amet consectetur adipisicing elit.', 'lifestyle','/images/photo-2.jpg', ''
+),
+(
+  3, 'seven reasons why coffee is awesome', 'Lorem ipsum dolor, sit amet consectetur adipisicing elit.', 'lifestyle', '/images/photo-3.jpg', ''
+),
+(
+  4, 'seven reasons why coffee is awesome', 'Lorem ipsum dolor, sit amet consectetur adipisicing elit.', 'lifestyle', '/images/photo-4.jpg', ''
+),
+(
+  5, 'seven reasons why coffee is awesome', 'Lorem ipsum dolor, sit amet consectetur adipisicing elit.', 'lifestyle', '/images/photo-5.jpg', ''
+),
+(
+  6, 'seven reasons why coffee is awesome', 'Lorem ipsum dolor, sit amet consectetur adipisicing elit.', 'lifestyle', '/images/photo-6.jpg', ''
+),
+(
+  7, 'seven reasons why coffee is awesome', 'Lorem ipsum dolor, sit amet consectetur adipisicing elit.', 'lifestyle', '/images/photo-7.jpg', ''
+),
+(
+  8, 'seven reasons why coffee is awesome', 'Lorem ipsum dolor, sit amet consectetur adipisicing elit.', 'lifestyle', '/images/photo-8.jpg', ''
+),
+(
+  9, 'seven reasons why coffee is awesome', 'Lorem ipsum dolor, sit amet consectetur adipisicing elit.', 'lifestyle', '/images/photo-9.jpg', ''
+)
+
---
+++ demo/w04_93_Blog/utils/database.js
@@ -0,0 +1,27 @@
+const { Pool } = require('pg');
+
+let pool;
+
+pool = new Pool({
+  host    : 'localhost',
+  port    : 5432,
+  user    : 'marxw',
+  password: "",
+  database: 'wp1_demo_93',
+});
+
+// console.log('Database connected', pool);
+
+// const db = require('./database');
+const testDB = async () => {
+  try {
+    const result = await pool.query(`SELECT * FROM card_93`);
+    console.log('json data: ', result.rows);
+  } catch (error) {
+    console.log(error);
+  };
+};
+
+testDB();
+
+module.exports = pool;
